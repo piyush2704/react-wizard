cec85b2a13054875bd2a05ed548213f34d4feb0c
piyush2704 <piyush.manchanda27@gmail.com>
2023-05-04 00:04:35 +0100

    form Feature

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,4 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+describe('App', () => {
+
+
+    it('should show only Next button if currely on first page', () => {
+        render(<App/>)
+        expect(screen.queryByText('Back')).toBeFalsy();
+    })
+    
+})
+
 
